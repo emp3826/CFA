02ab48ca4a04f939955b40b21001d05852b27756
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-10-01 11:07:33 +0800

    Update main.go

--- core/src/golang/clash/main.go
+++ core/src/golang/clash/main.go
@@ -14,8 +14,6 @@ import (
 	"github.com/Dreamacro/clash/hub"
 	"github.com/Dreamacro/clash/hub/executor"
 	"github.com/Dreamacro/clash/log"
-
-	"go.uber.org/automaxprocs/maxprocs"
 )
 
 var (
@@ -46,7 +44,6 @@ func init() {
 }
 
 func main() {
-	maxprocs.Set(maxprocs.Logger(func(string, ...any) {}))
 	if version {
 		fmt.Printf("Clash %s %s %s with %s %s\n", C.Version, runtime.GOOS, runtime.GOARCH, runtime.Version(), C.BuildTime)
 		return
