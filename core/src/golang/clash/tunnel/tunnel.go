da69ea6bb243bc0923f6e93d5f7f9734606c8c2e
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-10-04 12:30:26 +0800

    Update tunnel.go

--- core/src/golang/clash/tunnel/tunnel.go
+++ core/src/golang/clash/tunnel/tunnel.go
@@ -138,8 +138,6 @@ func preHandleMetadata(metadata *C.Metadata) error {
 				// redir-host should lookup the hosts
 				metadata.DstIP = node.Data.(net.IP)
 			}
-		} else if resolver.IsFakeIP(metadata.DstIP) {
-			return fmt.Errorf("fake DNS record %s missing", metadata.DstIP)
 		}
 	}
 
@@ -167,9 +165,6 @@ func handleUDPConn(packet *inbound.PacketAdapter) {
 
 	// make a fAddr if request ip is fakeip
 	var fAddr net.Addr
-	if resolver.IsExistFakeIP(metadata.DstIP) {
-		fAddr = metadata.UDPAddr()
-	}
 
 	if err := preHandleMetadata(metadata); err != nil {
 		return
