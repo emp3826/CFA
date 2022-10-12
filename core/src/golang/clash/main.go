11e38dba268817bd33c435f30926e50cbcfdf1b1
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-10-12 21:54:54 +0800

    Update main.go

--- core/src/golang/clash/main.go
+++ core/src/golang/clash/main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"flag"
 	"os"
 	"os/signal"
 	"syscall"
 )
-
-var (
-	flagset            map[string]bool
-	configFile         string
-	externalUI         string
-	externalController string
-	secret             string
-)
-
-func init() {
-	flag.StringVar(&configFile, "f", "", "specify configuration file")
-	flag.StringVar(&externalUI, "ext-ui", "", "override external ui directory")
-	flag.StringVar(&externalController, "ext-ctl", "", "override external controller address")
-	flag.StringVar(&secret, "secret", "", "override secret for RESTful API")
-	flag.Parse()
-
-	flagset = map[string]bool{}
-	flag.Visit(func(f *flag.Flag) {
-		flagset[f.Name] = true
-	})
-}
 
 func main() {
 	sigCh := make(chan os.Signal, 1)
