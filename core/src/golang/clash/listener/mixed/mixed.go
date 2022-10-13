d5442803a9f61057692a459282935b110ee8edcb
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-10-13 18:01:06 +0800

    Update mixed.go

--- core/src/golang/clash/listener/mixed/mixed.go
+++ core/src/golang/clash/listener/mixed/mixed.go
@@ -8,7 +8,6 @@ import (
 	N "github.com/Dreamacro/clash/common/net"
 	C "github.com/Dreamacro/clash/constant"
 	"github.com/Dreamacro/clash/listener/http"
-	"github.com/Dreamacro/clash/transport/socks5"
 )
 
 type Listener struct {
@@ -65,7 +64,7 @@ func handleConn(conn net.Conn, in chan<- C.ConnContext, cache *cache.Cache) {
 	conn.(*net.TCPConn).SetKeepAlive(true)
 
 	bufConn := N.NewBufferedConn(conn)
-	head, err := bufConn.Peek(1)
+
 	if err != nil {
 		return
 	}
