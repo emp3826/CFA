c93d883e24733e19590092252aaaa1187cea2d46
emp3826 <30045531+emp3826@users.noreply.github.com>
2023-01-21 14:44:08 +0800

    fix scale

--- core/src/main/cpp/bridge_helper.c
+++ core/src/main/cpp/bridge_helper.c
@@ -1,7 +1,7 @@
 #include "bridge_helper.h"
 
 uint64_t down_scale_traffic(uint64_t value) {
-    if (value > 1042 * 1024 * 1024)
+    if (value > 1024 * 1024 * 1024)
         return ((value * 100u / 1024u / 1024u / 1024u) & 0x3FFFFFFFu) | (3u << 30u);
     if (value > 1024 * 1024)
         return ((value * 100u / 1024u / 1024u) & 0x3FFFFFFFu) | (2u << 30u);
