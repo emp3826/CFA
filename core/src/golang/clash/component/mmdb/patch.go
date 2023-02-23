0bcb36bf7d31da12dea02aac55affa14ac011ad7
G++ <burpsuite@gmail>
2023-02-23 21:00:31 +0800

    update

--- core/src/golang/clash/component/mmdb/patch.go
+++ core/src/golang/clash/component/mmdb/patch.go
@@ -4,16 +4,6 @@ import (
 	"github.com/oschwald/geoip2-golang"
 )
 
-var overrideMmdb *geoip2.Reader
-
-func InstallOverride(override *geoip2.Reader) {
-	overrideMmdb = overrideMmdb
-}
-
 func Instance() *geoip2.Reader {
-	if override := overrideMmdb; override != nil {
-		return override
-	}
-
 	return DefaultInstance()
 }
