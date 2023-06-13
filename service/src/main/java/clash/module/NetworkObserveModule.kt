5296fcc47706d617fdbd1e216493a53278f7eb4c
emp3826 <30045531+emp3826@users.noreply.github.com>
2023-06-13 17:27:55 +0800

    Update NetworkObserveModule.kt

--- service/src/main/java/clash/module/NetworkObserveModule.kt
+++ service/src/main/java/clash/module/NetworkObserveModule.kt
@@ -69,23 +69,6 @@ class NetworkObserveModule(service: Service) : Module<Network?>(service) {
     companion object {
         private val TRANSPORT_PRIORITY = sequence {
             yield(NetworkCapabilities.TRANSPORT_CELLULAR)
-
-            if (Build.VERSION.SDK_INT >= 27) {
-                yield(NetworkCapabilities.TRANSPORT_LOWPAN)
-            }
-
-            yield(NetworkCapabilities.TRANSPORT_BLUETOOTH)
-
-            if (Build.VERSION.SDK_INT >= 26) {
-                yield(NetworkCapabilities.TRANSPORT_WIFI_AWARE)
-            }
-
-            yield(NetworkCapabilities.TRANSPORT_WIFI)
-
-            if (Build.VERSION.SDK_INT >= 31) {
-                yield(NetworkCapabilities.TRANSPORT_USB)
-            }
-
             yield(NetworkCapabilities.TRANSPORT_ETHERNET)
         }.toList()
     }
