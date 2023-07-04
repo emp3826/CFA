a152be67f893c902cbb20e81c39056e1d0382db3
G++ <burpsuite@gmail>
2023-07-04 17:29:15 +0800

    update

--- common/src/main/java/compat/Html.kt
+++ common/src/main/java/compat/Html.kt
@@ -2,14 +2,9 @@
 
 package com.github.kr328.clash.common.compat
 
-import android.os.Build
 import android.text.Html
 import android.text.Spanned
 
 fun fromHtmlCompat(content: String): Spanned {
-    return if (Build.VERSION.SDK_INT >= 24) {
-        Html.fromHtml(content, Html.FROM_HTML_MODE_COMPACT)
-    } else {
-        Html.fromHtml(content)
-    }
+    return Html.fromHtml(content, Html.FROM_HTML_MODE_COMPACT)
 }
