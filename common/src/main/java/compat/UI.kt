af97a37cefd2f9c1474f0e7a0749274662017734
G++ <burpsuite@gmail>
2023-07-05 10:37:44 +0800

    update

--- common/src/main/java/compat/UI.kt
+++ common/src/main/java/compat/UI.kt
@@ -2,7 +2,6 @@
 
 package com.github.kr328.clash.common.compat
 
-import android.annotation.TargetApi
 import android.os.Build
 import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
@@ -17,31 +16,13 @@ var Window.isSystemBarsTranslucentCompat: Boolean
         throw UnsupportedOperationException("set value only")
     }
     set(value) {
-        if (Build.VERSION.SDK_INT >= 30) {
-            setDecorFitsSystemWindows(!value)
+        setDecorFitsSystemWindows(!value)
+        if (value) {
+            attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         } else {
-            decorView.systemUiVisibility =
-                if (value) {
-                    decorView.systemUiVisibility or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                } else {
-                    decorView.systemUiVisibility and
-                            (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION).inv()
-                }
-        }
-
-        if (Build.VERSION.SDK_INT >= 28) {
-            if (value) {
-                attributes.layoutInDisplayCutoutMode =
-                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-            } else {
-                attributes.layoutInDisplayCutoutMode =
-                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
-            }
+            attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
         }
     }
 
@@ -49,31 +30,20 @@ var Window.isLightStatusBarsCompat: Boolean
     get() {
         throw UnsupportedOperationException("set value only")
     }
-    @TargetApi(23)
     set(value) {
         if (value) {
-            if (Build.VERSION.SDK_INT >= 30) {
-                decorView.windowInsetsController?.apply {
-                    setSystemBarsAppearance(
-                        APPEARANCE_LIGHT_STATUS_BARS,
-                        APPEARANCE_LIGHT_STATUS_BARS
-                    )
-                }
-            } else {
-                decorView.systemUiVisibility =
-                    decorView.systemUiVisibility or SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            decorView.windowInsetsController?.apply {
+                setSystemBarsAppearance(
+                    APPEARANCE_LIGHT_STATUS_BARS,
+                    APPEARANCE_LIGHT_STATUS_BARS
+                )
             }
         } else {
-            if (Build.VERSION.SDK_INT >= 30) {
-                decorView.windowInsetsController?.apply {
-                    setSystemBarsAppearance(
-                        0,
-                        APPEARANCE_LIGHT_STATUS_BARS
-                    )
-                }
-            } else {
-                decorView.systemUiVisibility =
-                    decorView.systemUiVisibility and SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+            decorView.windowInsetsController?.apply {
+                setSystemBarsAppearance(
+                    0,
+                    APPEARANCE_LIGHT_STATUS_BARS
+                )
             }
         }
     }
@@ -82,31 +52,20 @@ var Window.isLightNavigationBarCompat: Boolean
     get() {
         throw UnsupportedOperationException("set value only")
     }
-    @TargetApi(27)
     set(value) {
         if (value) {
-            if (Build.VERSION.SDK_INT >= 30) {
-                decorView.windowInsetsController?.apply {
-                    setSystemBarsAppearance(
-                        APPEARANCE_LIGHT_NAVIGATION_BARS,
-                        APPEARANCE_LIGHT_NAVIGATION_BARS
-                    )
-                }
-            } else {
-                decorView.systemUiVisibility =
-                    decorView.systemUiVisibility or SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            decorView.windowInsetsController?.apply {
+                setSystemBarsAppearance(
+                    APPEARANCE_LIGHT_NAVIGATION_BARS,
+                    APPEARANCE_LIGHT_NAVIGATION_BARS
+                )
             }
         } else {
-            if (Build.VERSION.SDK_INT >= 30) {
-                decorView.windowInsetsController?.apply {
-                    setSystemBarsAppearance(
-                        0,
-                        APPEARANCE_LIGHT_NAVIGATION_BARS
-                    )
-                }
-            } else {
-                decorView.systemUiVisibility =
-                    decorView.systemUiVisibility and SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+            decorView.windowInsetsController?.apply {
+                setSystemBarsAppearance(
+                    0,
+                    APPEARANCE_LIGHT_NAVIGATION_BARS
+                )
             }
         }
     }
