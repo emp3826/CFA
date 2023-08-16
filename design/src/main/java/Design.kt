70eaeab0bb5a186e5aad11cdefaf2f4fb0fb30a3
emp3826 <30045531+emp3826@users.noreply.github.com>
2023-08-16 18:18:41 +0800

    Update Design.kt

--- design/src/main/java/Design.kt
+++ design/src/main/java/Design.kt
@@ -5,11 +5,9 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kr328.clash.design.ui.Surface
 import com.github.kr328.clash.design.util.setOnInsertsChangedListener
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.withContext
 
 abstract class Design<R>(val context: Context) :
     CoroutineScope by CoroutineScope(Dispatchers.Unconfined) {
